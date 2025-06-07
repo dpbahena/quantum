833913314afed5e979b0c63527a028274445babb
dpbahena <dpbahena@gmail.com>
2025-06-06 21:40:53 -0500

    working quantum initial program

---
+++ build/CMakeFiles/bell.dir/bell.c.o.d
@@ -0,0 +1,65 @@
+CMakeFiles/bell.dir/bell.c.o: /home/dario/learning/quantum/bell.c \
+ /usr/include/stdc-predef.h /usr/include/stdio.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/timesize.h \
+ /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/stddef.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/stdarg.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/time64.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__fpos_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__mbstate_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__fpos64_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/cookie_io_functions_t.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn-common.h /usr/include/math.h \
+ /usr/include/x86_64-linux-gnu/bits/math-vector.h \
+ /usr/include/x86_64-linux-gnu/bits/libm-simd-decl-stubs.h \
+ /usr/include/x86_64-linux-gnu/bits/flt-eval-method.h \
+ /usr/include/x86_64-linux-gnu/bits/fp-logb.h \
+ /usr/include/x86_64-linux-gnu/bits/fp-fast.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls-helper-functions.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls.h \
+ /usr/local/cuda-12.6/targets/x86_64-linux/include/cuda_runtime.h \
+ /usr/local/cuda-12.6/targets/x86_64-linux/include/crt/host_config.h \
+ /usr/local/cuda-12.6/targets/x86_64-linux/include/builtin_types.h \
+ /usr/local/cuda-12.6/targets/x86_64-linux/include/device_types.h \
+ /usr/local/cuda-12.6/targets/x86_64-linux/include/crt/host_defines.h \
+ /usr/local/cuda-12.6/targets/x86_64-linux/include/driver_types.h \
+ /usr/local/cuda-12.6/targets/x86_64-linux/include/vector_types.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/limits.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/syslimits.h \
+ /usr/include/limits.h /usr/include/x86_64-linux-gnu/bits/posix1_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/local_lim.h \
+ /usr/include/linux/limits.h \
+ /usr/include/x86_64-linux-gnu/bits/pthread_stack_min-dynamic.h \
+ /usr/include/x86_64-linux-gnu/bits/pthread_stack_min.h \
+ /usr/include/x86_64-linux-gnu/bits/posix2_lim.h \
+ /usr/local/cuda-12.6/targets/x86_64-linux/include/surface_types.h \
+ /usr/local/cuda-12.6/targets/x86_64-linux/include/texture_types.h \
+ /usr/local/cuda-12.6/targets/x86_64-linux/include/library_types.h \
+ /usr/local/cuda-12.6/targets/x86_64-linux/include/channel_descriptor.h \
+ /usr/local/cuda-12.6/targets/x86_64-linux/include/cuda_runtime_api.h \
+ /usr/local/cuda-12.6/targets/x86_64-linux/include/cuda_device_runtime_api.h \
+ /usr/local/cuda-12.6/targets/x86_64-linux/include/driver_functions.h \
+ /usr/local/cuda-12.6/targets/x86_64-linux/include/vector_functions.h \
+ /usr/local/cuda-12.6/targets/x86_64-linux/include/vector_functions.hpp \
+ /usr/local/cuda-12.6/targets/x86_64-linux/include/cuComplex.h \
+ /usr/local/cuda-12.6/targets/x86_64-linux/include/custatevec.h \
+ /usr/local/cuda-12.6/targets/x86_64-linux/include/library_types.h \
+ /usr/local/cuda-12.6/targets/x86_64-linux/include/cuda_runtime_api.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/stdint.h /usr/include/stdint.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-intn.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-least.h
